cfa18184e70d7b4901a294c72c2ec263dcca9ea8
dipti-biswas-ship-it <diptibiswas453@gmail.com>
2025-06-16 13:06:09 +0530

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-
-# from .database import engine, Base, SessionLocal
 from .routes import router as booking_router
 from fastapi import FastAPI
 from . import models
 from .database import engine
-# Base.metadata.create_all(bind=engine)
+
+# Automatically create tables in the database using SQLAlchemy models
 models.Base.metadata.create_all(bind=engine)
 
-
+# Create the FastAPI app
 app = FastAPI(title="Fitness Booking API")
-app.include_router(booking_router)
 
+# Include the booking routes (class and booking endpoints)
+app.include_router(booking_router)
